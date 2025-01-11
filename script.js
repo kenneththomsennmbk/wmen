d347f596731ae72d98bda45e78067e46e9aed849
kenneththomsennmbk <54923832+kenneththomsennmbk@users.noreply.github.com>
2025-01-10 16:49:45 -0800

    This is a replacement for the current script file..
    This takes into account that the value of the const var is empty and only a refresh of the browser will make the values stick. 
    So i utilized a button to update the values of the now variable vars and send the user-data and return nothing after the button has been clicked..
    Much better than what we started with.

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-const myForm = document.getElementById("myForm");
-const nombre = document.getElementById("nombre").value;
-const surname = document.getElementById("surname").value;
-const email = document.getElementById("e").value;
-const userObj = {name: `${nombre}`, surname: `${surname}`, email: `${email}`};
- const request = new Request("http://localhost:3000/thank-you.html", { method: "post", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(userObj)});
+var button = document.getElementById("button-one");	
+button.addEventListener("click", getSendData);
+function getSendData() {
+	if(button.value == "not-submitted") {
+var myForm = document.getElementById("myForm");
+var nombre = document.getElementById("nombre").value;
+var surname = document.getElementById("surname").value;
+var email = document.getElementById("e").value;
+
+var userObj = {name: `${nombre}`, surname: `${surname}`, email: `${email}`};
+
+ const request = new Request("http://localhost:3000/thank-you.html", { method: "post", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(userObj)});//used to be userObj as stringify param
+button.value = "submitted";
+		sendData();
+	} else { button.value == "submitted"; 
+		return 0;
+	}
 async function sendData() {
-	request;
 	try {
-		fetch(request, {
+		fetch("http://localhost:3000/thank-you.html", {
 			method: "post",
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify(userObj)
 		});
-		console.log(await request);
 	} catch(e) {
 		console.error(e);
 	}
 }
-	
+};
 myForm.addEventListener("submit", (event) => {
 	event.preventDefault();
-	sendData();
+	getSendData();
 });
